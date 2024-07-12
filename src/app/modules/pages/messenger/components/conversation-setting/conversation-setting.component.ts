9f371080aaa4939e7761136d24c00400c6616689
hiu_cd <hovanhieu2012sch@gmail.com>
2024-07-12 21:22:02 +0700

    conversation setting

--- src/app/modules/pages/messenger/components/conversation-setting/conversation-setting.component.ts
+++ src/app/modules/pages/messenger/components/conversation-setting/conversation-setting.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Image from 'src/app/common/interface/image.interface';
 
 @Component({
   selector: 'app-conversation-setting',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./conversation-setting.component.scss']
 })
 export class ConversationSettingComponent {
+  images : Image[] =  [
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 1' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 2' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 3' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 1' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 2' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 3' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 1' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 2' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 3' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 1' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 2' },
+    { src: '../../../../../../assets/san-marco.jpg', alt: 'photo 3' }
+  ];
 
+  visibleImages : Image [] = [];
+  showLimit = 3; 
+
+  constructor() {
+    this.updateVisibleImages();
+  }
+
+  updateVisibleImages() {
+    this.visibleImages = this.images.slice(0, this.showLimit);
+  }
+
+  showMore() {
+    this.showLimit += 3;
+    this.updateVisibleImages();
+  }
 }
