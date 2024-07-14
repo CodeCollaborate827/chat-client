cdded8ecab9fbbdb20b9a9ed8b6cd61a68ab6378
hiu_cd <hovanhieu2012sch@gmail.com>
2024-07-14 16:32:51 +0700

    err - declaration layout

--- src/app/modules/layout/components/aside/aside.component.ts
+++ src/app/modules/layout/components/aside/aside.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.scss']
+  styleUrls: ['./aside.component.css']
 })
 export class AsideComponent {
+  activeTab = 'Chat'; 
 
+  setActiveTab(tabName: string) {
+    this.activeTab = tabName;
+  }
 }
--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LayoutComponent } from "./layout.component";
 import { AsideComponent, FooterComponent } from "./components";
+import { MatTabsModule } from '@angular/material/tabs';
+import { MessengerComponent } from "../pages/messenger/messenger.component";
+import { PagesComponent } from "../pages/pages.component";
 
 @NgModule({
   declarations: [
     LayoutComponent,
     AsideComponent,
-    FooterComponent
+    FooterComponent,
+    MessengerComponent
   ],
   imports: [
     CommonModule,
+    MatTabsModule,
   ],
   exports: [
     LayoutComponent
